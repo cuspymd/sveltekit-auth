8639f095fd13fe26b0889b907b7c4b3e7deb86f9
Myoungdo Park <cuspymd@gmail.com>
2022-01-09 16:15:32 +0000

    Add end points for authentication

---
+++ src/routes/api/sign-in.js
@@ -0,0 +1,32 @@
+import { createSession, getUserByEmail } from './_db';
+import { serialize } from 'cookie';
+
+export async function post({ body: { email, password } }) {
+    const user = await getUserByEmail(email);
+
+    if (!user || user.password !== password) {
+        return {
+            status: 401,
+            body: {
+                message: 'Incorrect user or password'
+            }
+        };
+    }
+
+    const { id } = await createSession(email);
+    return {
+        status: 200,
+        Headers: {
+            'Set-Cookie': serialize('session_id', id, {
+                path: '/',
+                httpOnly: true,
+                sameSite: 'strict',
+                secure: process.env.NODE_ENV === 'production',
+                maxAge: 60 * 60 * 24 * 7 // one week
+            })
+        },
+        body: {
+            message: 'Sucessfully signed in'
+        }
+    };
+}
